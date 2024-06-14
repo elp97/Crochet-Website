e24ab1e0cb7a95ab48e93dc8dddd53e9dd64d350
elp97 <68276669+elp97@users.noreply.github.com>
2024-06-14 17:09:40 +0100

    feat(api): unit tests added
    Unit tests added using xUnit, FluentAssertions, FakeItEasy and Microsoft.EntityFrameworkCore.InMemory.

---
+++ Crochet-Website.ServerTests/Controller/ProductControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Crochet_Website.Server.Controllers;
+using Crochet_Website.Server.Interfaces;
+using Crochet_Website.Server.Models;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crochet_Website.ServerTests.Controller
+{
+    public class ProductControllerTests
+    {
+        private ProductController _productController;
+        private IProductRepository _productRepo;
+        public ProductControllerTests()
+        {
+            _productRepo = A.Fake<IProductRepository>();
+            _productController = new ProductController(_productRepo);
+        }
+
+        [Fact]
+        public void ProductController_GetAll_ReturnOk()
+        {
+            //Act
+            var products = A.Fake<List<Product>>();
+            A.CallTo(() => _productRepo.GetAllAsync()).Returns(products);
+
+            //Arrange
+            var result = _productController.GetAll();
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+    }
+}
---
+++ Crochet-Website.ServerTests/Repository/ProductRepositoryTests.cs
@@ -0,0 +1,73 @@
+﻿using Crochet_Website.Server.Data;
+using Crochet_Website.Server.Interfaces;
+using Crochet_Website.Server.Models;
+using Crochet_Website.Server.Repository;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crochet_Website.ServerTests.Repository
+{
+    public class ProductRepositoryTests
+    {
+        private async Task<DataContext> GetDBContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+              .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+              .EnableSensitiveDataLogging()
+              .Options;
+            var databaseContext = new DataContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Products.CountAsync() <= 0)
+            {
+                for (int i = 1; i <= 11; i++)
+                {
+                    Product product = new Product()
+                    {
+                        ProductID = i,
+                        Name = "Test Product",
+                        Type = "Test",
+                        Price = 99.99,
+                        Description_Small = "Small description",
+                        Description_Long = "Long Description"
+                    };
+                    databaseContext.Products.Add(product);
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            if (await databaseContext.Images.CountAsync() <= 0)
+            {
+                Image image = new Image
+                {
+                    ImageID = 1,
+                    ProductID = 1,
+                    ImageURL = "test.png"
+                };
+                databaseContext.Images.Add(image);
+                await databaseContext.SaveChangesAsync();
+            }
+            return databaseContext;
+
+        }
+
+        [Fact]
+        public async void ProductRepository_GetAllAsync_ReturnProduct()
+        {
+            //Act
+            var dbContext = await GetDBContext();
+            var productRepo = new ProductRepository(dbContext);
+
+            //Arrange
+            var result = productRepo.GetAllAsync();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<List<Product>>>();
+        }
+    }
+}
